465c59a551b9ae34382c2fe56d8d4cc07beb415a
AyoubOuazizi <101675808+AyoubOuazizi@users.noreply.github.com>
2022-03-26 22:56:38 +0100

    Add files via upload

--- src/application/New_adminController.java
+++ src/application/New_adminController.java
@@ -21,7 +21,11 @@ public class New_adminController {
 	@FXML
 	private PasswordField passwd;
 	@FXML
+	private PasswordField passwd2;
+	@FXML
 	private TextField name;
+	@FXML
+	private Label error;
 	
 	@FXML
 	private void initialize() {
@@ -44,14 +48,22 @@ public class New_adminController {
 	}
 	
 	public void newAdmin() throws IOException {
-		try (FileOutputStream fos = new FileOutputStream("admin",true);
-			 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-			    Dentiste dentiste = new Dentiste(login.getText(),passwd.getText(),name.getText());
-			    oos.writeObject(dentiste);
-			} catch (IOException ex) {
-			    ex.printStackTrace();
-			}
-		Main main = new Main();
-		main.changeScene("Sign_in.fxml");
+		if (!passwd.equals(passwd2)) {
+			error.setText("Veuillez confirmer votre password");
+		}
+		else if (name.equals(null) || login.equals(null) || passwd.equals(null) || passwd2.equals(null)) {
+			error.setText("Tous les champs sont obligatoires");
+		}
+		else {
+			try (FileOutputStream fos = new FileOutputStream("admin",true);
+				 ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+				    Dentiste dentiste = new Dentiste(login.getText(),passwd.getText(),name.getText());
+				    oos.writeObject(dentiste);
+				} catch (IOException ex) {
+				    ex.printStackTrace();
+				}
+			Main main = new Main();
+			main.changeScene("Sign_in.fxml");
+		}
 	}
 }
